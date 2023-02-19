318f5591606931f15d7a8ecdda924fe62e759300
rethfupy <solamdev@outlook.com>
2023-02-19 15:34:51 +0200

    add feature
    add function to change color of background

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function changeBrightness() {
     let brightness = document.getElementById('brightness').value;
     let background = document.querySelector('.opacity');
+    let color = document.getElementById('color').value;
     
+    background.style.backgroundColor = color;
     background.style.opacity = brightness / 100;
 }
